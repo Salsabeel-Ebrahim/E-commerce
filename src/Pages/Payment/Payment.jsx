8647dd195158ce6c5ea632bca926d27a7cc7fe16
Salsabeel Ebrahim <salsabeelibrahem03@gmail.com>
2025-07-11 13:40:38 +0300

    fix: resolved conflict and successful build

--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -40,13 +40,8 @@ async function online(values){
    const test = {
     shippingAddress : values }
      const options ={
-<<<<<<< HEAD
-        url:`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartInfo?.cartId}?url=https://salsabeel-ebrahim.github.io/E-commerce/` , 
-        
-=======
+
         url:`https://ecommerce.routemisr.com/api/v1/orders/checkout-session/${cartInfo?.cartId}?url=http://localhost:5173` , 
-        // https://salsabeel-ebrahim.github.io/E-commerce/
->>>>>>> cca8dba (fix: adjusted paths, fonts, and Vercel base config)
 
         method :"POST" ,
         headers :{
